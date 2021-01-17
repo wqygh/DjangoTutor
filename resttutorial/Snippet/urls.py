2144b2d68b7f41a1690fc9d9a47448a6725d332b
QW <wqysbu@gmail.com>
2021-01-17 00:51:31 -0700

    rest_tutor

--- resttutorial/Snippet/urls.py
+++ resttutorial/Snippet/urls.py
@@ -3,8 +3,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from Snippet import views
 
 urlpatterns = [
-    path('Snippet/', views.SnippetList.as_view()),
-    path('Snippet/<int:pk>/', views.SnippetDetail.as_view()),
+    path('Snippet/', views.SnippetList.as_view(), name='snippet-list'),
+    path('Snippet/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('Snippet/<int:pk>/highlight/', 
+         views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
