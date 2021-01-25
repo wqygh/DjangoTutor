a40f15b7a9677f9a0f2e62a4ddb572f9602c6552
QW <wqysbu@gmail.com>
2021-01-24 21:49:20 -0700

    before custom template tags

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -23,6 +23,7 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     #re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
